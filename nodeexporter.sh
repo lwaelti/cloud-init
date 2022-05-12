c6d428e02ae6df7345103ffca5a2260714768c76
lwaelti <86109793+lwaelti@users.noreply.github.com>
2022-05-12 20:32:11 +0200

    update

--- nodeexporter.sh
+++ nodeexporter.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-version="${VERSION:-1.0.1}"
-arch="${ARCH:-linux-amd64}"
-bin_dir="${BIN_DIR:-/usr/local/bin}"
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz"
 
-wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" \
-    -O /tmp/node_exporter.tar.gz
-
-mkdir -p /tmp/node_exporter
-
-cd /tmp || { echo "ERROR! No /tmp found.."; exit 1; }
-
-tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERROR! Extracting the node_exporter tar"; exit 1; }
-
-cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
-chown root:staff "$bin_dir/node_exporter"
+tar xfz node_exporter.tar.gz
 
 cat <<EOF > /etc/systemd/system/node_exporter.service
 [Unit]

6e221985823580e904739e8f19cc9158c88ab9f3
lwaelti <86109793+lwaelti@users.noreply.github.com>
2022-05-12 22:36:50 +0200

    update

--- script.sh
+++ script.sh
@@ -15,5 +15,20 @@ sudo apt-get install -y docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
-sudo docker run -d --name prometheus-container -e TZ=CET -p 30090:9090 ubuntu/prometheus:2.33-22.04_beta
+
+cat <<'EOF'>/home/dockerroot/prometheus.yml
+global:
+  scrape_interval:     10s
+  evaluation_interval: 10s
+rule_files:
+  # - "first.rule"
+  # - "second.rule"
+scrape_configs:
+  - job_name: prometheus
+    static_configs:
+      - targets: ['localhost:9090']
+EOF
+
+sudo docker run -d -p 9090:9090 -v /home/dockerroot/prometheus.yml:/etc/prometheus/prometheus.yml --name=prometheus prom/prometheus
+#sudo docker run -d --name prometheus-container -e TZ=CET -p 30090:9090 ubuntu/prometheus:2.33-22.04_beta
 sudo docker run -d --name grafana-container -e TZ=CET -p 3000:3000 grafana/grafana-oss

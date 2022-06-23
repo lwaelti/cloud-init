41522142effa162f288818515ff31fc39ec466ef
lwaelti <86109793+lwaelti@users.noreply.github.com>
2022-06-23 11:06:55 +0200

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -3,6 +3,5 @@
 apt update
 apt upgrade
 apt install -y boinc
-usermod -a -G boinc $USER
-exec su $USER
+chmod 774 /etc/boinc-client/gui_rpc_auth.cfg
 boinccmd --project_attach https://einsteinathome.org. f5fe866540880f6791a60e87f43cd403

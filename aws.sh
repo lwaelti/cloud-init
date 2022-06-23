1fb65a4415dac4cd9044c4e6520676f7384027a6
lwaelti <86109793+lwaelti@users.noreply.github.com>
2022-06-23 10:31:44 +0200

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade
-sudo apt install boinc
+sudo apt install -y boinc
 sudo usermod -a -G boinc $USER
 exec su $USER
 boinccmd --project_attach https://einsteinathome.org. f5fe866540880f6791a60e87f43cd403
